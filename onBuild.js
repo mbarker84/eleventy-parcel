da5880e8bbb5b3b88bfa193289d3725416a8ddce
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-11-30 22:04:15 +0000

    Content based hashing

--- onBuild.js
+++ onBuild.js
@@ -1,19 +1,57 @@
-const fs = require('fs');
+const fs = require('fs')
+const util = require('util')
+const md5 = require('md5')
 
-let hash = Date.now()
-hash = hash.toString()
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
 
-fs.writeFile('src/_data/version.json', hash, function (err) {
-  if (err) return console.log(err);
-  console.log(`${hash} > src/_data/version.json`);
-});
+let hashCss
+let hashJs
 
-fs.rename('dist/styles.css', `dist/styles${hash}.css`, function(err) {
-  if ( err ) console.log('ERROR: ' + err);
-  console.log(`dist/styles.css > dist/styles${hash}.css`);
-});
+const readCssFile = () => {
+  return readFile('dist/styles.css', (err, data) => {
+    if (err) console.log(err)
+  })
+}
 
-fs.rename('dist/index.js', `dist/index${hash}.js`, function(err) {
-  if ( err ) console.log('ERROR: ' + err);
-  console.log(`dist/index.js > dist/index${hash}.js`);
-});
+const readJsFile = () => {
+  return readFile('dist/index.js', (err, data) => {
+    if (err) console.log(err)
+  })
+}
+
+readCssFile()
+  .then((data) => {
+    hashCss = md5(data).slice(0, 16)
+    console.log(hashCss)
+  })
+  .then(() => {
+    readJsFile()
+      .then((data) => {
+        hashJs = md5(data).slice(0, 16)
+        console.log(hashJs)
+      })
+      .then(() => {
+        const versionObject = {
+          css: hashCss,
+          js: hashJs
+        }
+
+        writeFile('src/_data/version.json', JSON.stringify(versionObject), (err) => {
+          if (err) throw err
+          console.log('The file has been saved!')
+        })
+      })
+      .then(() => {
+        fs.rename('dist/styles.css', `dist/styles${hashCss}.css`, function(err) {
+          if ( err ) return console.log('ERROR: ' + err)
+          console.log(`dist/styles.css > dist/styles${hashCss}.css`)
+        })
+
+        fs.rename('dist/index.js', `dist/index${hashJs}.js`, function(err) {
+          if ( err ) return console.log('ERROR: ' + err)
+          console.log(`dist/index.js > dist/index${hashJs}.js`)
+        })
+      })
+  })
+
